ba2f39bd1cad9332095766f48f9211dcc494c945
santosh <singhsanty143@gmail.com>
2025-12-23 01:52:13 +0530

    handle google commented

--- tests/handleautosuggesion.spec.js
+++ tests/handleautosuggesion.spec.js
@@ -1,7 +1,31 @@
-const { test, expect } = require('@playwright/test');
-test.use({viewport:{width:1920,height:1080}})
+// const { test, expect } = require('@playwright/test');
+// test.use({viewport:{width:1920,height:1080}})
 
-// test('valid Application Title', async ({ page }) => {
+// // test('valid Application Title', async ({ page }) => {
+
+// //   await page.goto('https://www.google.com');
+
+// //   // Enter username
+// //   await page.locator("textarea[name='q']").fill("Mukesh Otwani");
+
+// //   // Enter password
+// //   await page.waitForSelector("//li[@role='presentation'] ")
+
+// //   await page.keyboard.press("ArrowDown")
+  
+// //   await page.waitForTimeout(5000)
+
+// //   await page.keyboard.press("ArrowDown")
+  
+// //   await page.waitForTimeout(5000)
+
+// //   await page.keyboard.press("Enter")
+
+// //   await page.waitForTimeout(10000)
+
+// // });
+
+// test('Autosuggesion Random select', async ({ page }) => {
 
 //   await page.goto('https://www.google.com');
 
@@ -9,42 +33,18 @@ test.use({viewport:{width:1920,height:1080}})
 //   await page.locator("textarea[name='q']").fill("Mukesh Otwani");
 
 //   // Enter password
-//   await page.waitForSelector("//li[@role='presentation'] ")
+//   await page.waitForSelector("//div[@id='Alh6id'] ")
 
-//   await page.keyboard.press("ArrowDown")
-  
-//   await page.waitForTimeout(5000)
-
-//   await page.keyboard.press("ArrowDown")
-  
-//   await page.waitForTimeout(5000)
-
-//   await page.keyboard.press("Enter")
-
-//   await page.waitForTimeout(10000)
-
-// });
-
-test('Autosuggesion Random select', async ({ page }) => {
-
-  await page.goto('https://www.google.com');
-
-  // Enter username
-  await page.locator("textarea[name='q']").fill("Mukesh Otwani");
-
-  // Enter password
-  await page.waitForSelector("//div[@id='Alh6id'] ")
-
-  const elements = await page.$$("//div[@id='Alh6id']")
-  for(let i=0;i<elements.length;i++)
-  {
-    const text= await elements[i].textContent()
-    if(text.includes('youtube'))
-    {
-        await elements[i].click()
-        break
-    }
+//   const elements = await page.$$("//div[@id='Alh6id']")
+//   for(let i=0;i<elements.length;i++)
+//   {
+//     const text= await elements[i].textContent()
+//     if(text.includes('youtube'))
+//     {
+//         await elements[i].click()
+//         break
+//     }
     
-}
-    await page.waitForTimeout(6000)
-});
+// }
+//     await page.waitForTimeout(6000)
+// });
