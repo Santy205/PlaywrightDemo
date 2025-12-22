322223ab59585ad786c0117d6d04fe07cb234822
santosh <singhsanty143@gmail.com>
2025-12-23 00:11:09 +0530

    new2

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -9,17 +9,19 @@ test('valid login', async ({ page }) => {
 
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-  // Enter username
   await page.fill('input[name="username"]', 'Admin');
 
-  // Enter password
   await page.fill('input[name="password"]', 'admin123');
 
-  // Click login
   await page.click('button[type="submit"]');
 
-  await page.waitForTimeout(5000)
+  await page.waitForTimeout(5000);
 
   // Assertion: Dashboard should be visible
   await expect(page.locator('h6.oxd-text')).toHaveText('Dashboard');
+  await page.click('span:has-text("My Info")');
+  await page.locator("//div[@class='orangehrm-horizontal-padding orangehrm-vertical-padding']//div[@class='oxd-grid-3 orangehrm-full-width-grid']//div[1]//div[1]//div[2]//div[1]//div[1]//div[2]//i[1]").click();
+  await page.locator(':text("Chinese")').click()
+
+
 });
