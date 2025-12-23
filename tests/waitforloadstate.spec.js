3b37d80f22c0f4c124596bd92f64124bc28803ff
santosh <singhsanty143@gmail.com>
2025-12-23 22:06:51 +0530

    commented

--- tests/waitforloadstate.spec.js
+++ tests/waitforloadstate.spec.js
@@ -1,9 +1,9 @@
-// const{test,expect}= require('@playwright/test')
-// test("wait for load state",async function ({page}) {
-//     await page.goto("https://freelance-learn-automation.vercel.app/login")
-//     await page.getByText("New User? Signup").click()
-//     await page.waitForLoadState("networkidle")
-//     const count=await page.locator("//input[@type='checkbox']").count()
-//     expect(count).toBe(6)
-// })
-// // checking
+const{test,expect}= require('@playwright/test')
+test("wait for load state",async function ({page}) {
+    await page.goto("https://freelance-learn-automation.vercel.app/login")
+    await page.getByText("New User? Signup").click()
+    await page.waitForLoadState("networkidle")
+    const count=await page.locator("//input[@type='checkbox']").count()
+    expect(count).toBe(6)
+})
+// checking
