1f691f2701f50a4c0f824879fb1a421eda0f77ab
santosh <singhsanty143@gmail.com>
2025-12-28 00:32:47 +0530

    timeout added

--- tests/waitforloadstate.spec.js
+++ tests/waitforloadstate.spec.js
@@ -4,7 +4,7 @@ test("wait for load state",async function ({page}) {
     await page.getByText("New User? Signup").click()
     await page.waitForLoadState("networkidle")
     const count=await page.locator("//input[@type='checkbox']").count()
-    await page.waitForLoadState("networkidle")
+    await page.waitForTimeout(2000)
 
     expect(count).toBe(4)
 })
