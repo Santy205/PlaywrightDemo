7193fcf56d1926c9ec2afec4febc3c5c3afba71c
santosh <singhsanty143@gmail.com>
2026-01-06 20:28:40 +0530

    checking

--- tests/waitforloadstate.spec.js
+++ tests/waitforloadstate.spec.js
@@ -3,10 +3,10 @@ test("wait for load state",async function ({page}) {
     await page.goto("https://freelance-learn-automation.vercel.app/login")
     await page.getByText("New User? Signup").click()
     await page.waitForLoadState("networkidle")
-    const count=await page.locator("//input[@type='checkbox']").count()
-    await page.waitForTimeout(2000)
+    const count=await page.locator("//input[@class='form-check-input']").count()
+    await page.waitForTimeout(9000)
 
-    //expect(count).toBe(14)
+    expect(count).toBe(6)
     
 })
 // checking
