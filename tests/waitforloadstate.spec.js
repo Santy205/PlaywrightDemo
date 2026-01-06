2efc454fa71a5775734a79563034951c7e45ea0e
santosh <singhsanty143@gmail.com>
2026-01-06 20:42:36 +0530

    coment removed

--- tests/waitforloadstate.spec.js
+++ tests/waitforloadstate.spec.js
@@ -3,10 +3,9 @@ test("wait for load state",async function ({page}) {
     await page.goto("https://freelance-learn-automation.vercel.app/login")
     await page.getByText("New User? Signup").click()
     await page.waitForLoadState("networkidle")
-    const count=await page.locator("//input[@class='form-check-input']").count()
+    const countcheckbox=await page.locator("//input[@class='form-check-input']").count()
     await page.waitForTimeout(9000)
-
-    expect(count).toBe(6)
-    
+    const countradio=await page.locator("//input[@type='radio']").count()
+    expect(countcheckbox).toBe(6)
+    expect(countradio).toBe(2)
 })
-// checking
